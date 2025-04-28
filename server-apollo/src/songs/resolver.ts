f44bae7dc0661fb425899b4f96f17ca8b6d8bffd
gwenael9 <gwenaelgueho@gmail.com>
2025-04-28 11:10:53 +0200

    Ajout dans le resolver du song By ID

--- server-apollo/src/songs/resolver.ts
+++ server-apollo/src/songs/resolver.ts
@@ -6,6 +6,9 @@ export const songResolvers: Resolvers = {
     songs: (_, __, { dataSources }) => {
       return dataSources.db.song.findMany();
     },
+    song: (_, { id }, { dataSources }) => {
+      return dataSources.db.song.findById(id);
+    },
   },
   Song: {
     user: async (parent, _, { dataSources }) => {
