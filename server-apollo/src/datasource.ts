4f13064828956debf5f265bd89f642ceba857f8c
Alexis Frangul <alexis.frangul@efrei.net>
2025-04-28 15:09:02 +0200

    feat : add a Genre template to group songs of the same genre, create the schema, mocked data, resolvers, mutations, relationships and add an optional argument to the songs query to filter by genre

--- server-apollo/src/datasource.ts
+++ server-apollo/src/datasource.ts
@@ -1,4 +1,4 @@
-import { users, songs } from "./mock-data";
+import { users, songs, genres} from "./mock-data";
 import { FakeORM, DBModel, FakeDataSource } from "./FakeORM";
 
 export interface DBUser extends DBModel {
@@ -8,14 +8,21 @@ export interface DBUser extends DBModel {
 export interface DBSong extends DBModel {
   name: string;
   userId: string;
+  genreId: string;
+}
+
+export interface DBGenre extends DBModel {
+  name: string;
 }
 
 export interface Database extends FakeDataSource {
   user: FakeORM<DBUser>;
   song: FakeORM<DBSong>;
+  genre: FakeORM<DBGenre>;
 }
 
 export const database: Database = {
   user: new FakeORM<DBUser>("user", users),
   song: new FakeORM<DBSong>("song", songs),
+  genre: new FakeORM<DBGenre>("genre", genres),
 };
