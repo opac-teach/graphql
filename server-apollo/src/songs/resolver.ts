d50470ad6f7480ee8d9e2cb97c51f6ce046ad33a
matteobnvl <matteobonneval19@gmail.com>
2025-04-28 10:49:25 +0200

    [FEAT] songs graphql

--- server-apollo/src/songs/resolver.ts
+++ server-apollo/src/songs/resolver.ts
@@ -1,10 +1,19 @@
 import { GraphQLError } from "graphql";
 import { Resolvers } from "../types";
+import {DBSong} from "../datasource";
 
 export const songResolvers: Resolvers = {
   Query: {
     songs: (_, __, { dataSources }) => {
       return dataSources.db.song.findMany();
     },
+    song: (_: {}, { id }, { dataSources }) => {
+      return dataSources.db.song.findById(id)
+    }
   },
-};
+  Song: {
+    user: async (parent: DBSong, _ : {}, { dataSources }): Promise<any> => {
+      return dataSources.db.user.findById(parent.userId)
+    }
+  }
+}
