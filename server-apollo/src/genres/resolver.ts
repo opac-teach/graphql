6a2da488679a3a331de70dc471d1ecb1e47de3d5
Alexis Frangul <alexis.frangul@efrei.net>
2025-05-08 22:46:33 +0200

    feat : use zod to validate mutation entries.

--- server-apollo/src/genres/resolver.ts
+++ server-apollo/src/genres/resolver.ts
@@ -1,15 +1,55 @@
 import { GraphQLError } from "graphql";
 import { Resolvers } from "../types";
+import { z } from "zod";
+
+const createGenreInputSchema = z.object({
+  name: z.string().min(3),
+})
+
+const genreIdParamSchema = z.string().uuid();
+
+const genresPaginationSchema = z.object({
+  page: z.number(),
+  pageSize: z.number(),
+})
 
 export const genreResolvers: Resolvers = {
   Query: {
     genres: (_, { pagination }, { dataSources }) => {
+      if (pagination) {
+        try {
+          genresPaginationSchema.parse(pagination);
+        } catch (error) {
+          throw new GraphQLError(
+            `${error}`,
+            {
+              extensions: {
+                code: "BAD_USER_INPUT",
+              }
+            }
+          )
+        }
+      }
+
       return dataSources.db.genre.findMany(_, pagination ? {
         limit: pagination.pageSize,
         offset: pagination.page * pagination.pageSize
       } : {});
     },
     genre: (_, { id }, { dataSources }) => {
+      try {
+        genreIdParamSchema.parse(id);
+      } catch (error) {
+        throw new GraphQLError(
+          `${error}`,
+          {
+            extensions: {
+              code: "BAD_USER_INPUT",
+            }
+          }
+        )
+      }
+
       return dataSources.db.genre.findById(id);
     },
   },
@@ -35,6 +75,19 @@ export const genreResolvers: Resolvers = {
             },
           }
         );
+      }
+
+      try {
+        createGenreInputSchema.parse(input);
+      } catch (error) {
+        throw new GraphQLError(
+          `${error}`,
+          {
+            extensions: {
+              code: "BAD_USER_INPUT",
+            }
+          }
+        )
       }
 
       try {
